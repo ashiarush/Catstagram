6623ef560f56dbb4b71f1bd2845b908f1b076715
Alok Gupta <Alok.Gupta@excelian.com>
2020-04-22 23:01:21 +0530

    Adding Catstagram Identity And Cat Models

---
+++ Catstagram.Server/Controllers/CatsController.cs
@@ -0,0 +1,15 @@
+﻿namespace Catstagram.Server.Controllers
+{
+    using Catstagram.Server.Models.Cats;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+    public class CatsController : ApiController
+    {
+        //[Authorize]
+        //[HttpPost]
+        //public async Task<ActionResult<int>> Create(CreateCatRequestModel model)
+        //{ 
+        //}
+    }
+}
